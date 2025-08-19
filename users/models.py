4126c3fd0b8857767b611ec44d57b9c074c1b5fc
Mesh-254 <meshack1995@gmail.com>
2025-08-19 16:45:40 +0300

    set is_active to false to enable user to confirm account

--- users/models.py
+++ users/models.py
@@ -54,7 +54,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     role = models.CharField(max_length=20, choices=Role.choices, default=Role.CUSTOMER)
 
     # Django admin flags
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
 
     date_joined = models.DateTimeField(default=timezone.now)
