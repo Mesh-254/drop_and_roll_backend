ac16c0c0b6772ba22a306151cf4b3a12650ea8c3
homemix <kenmutati@gmail.com>
2025-08-19 10:33:56 +0300

    ADD: Initial admin UI configs.

--- users/admin.py
+++ users/admin.py
@@ -1,13 +1,17 @@
-from django.contrib import admin
+
 from django.contrib.auth import get_user_model
+from django.contrib import admin
+from unfold.admin import ModelAdmin
 
 from .models import CustomerProfile, DriverProfile, AdminProfile, DriverDocument, DriverInvitation
 
 User = get_user_model()
 
 
+
+
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(ModelAdmin):
     list_display = ("email", "full_name", "role", "is_active", "date_joined")
     list_filter = ("role", "is_active", "date_joined")
     search_fields = ("email", "full_name", "phone")
@@ -15,28 +19,28 @@ class UserAdmin(admin.ModelAdmin):
 
 
 @admin.register(DriverProfile)
-class DriverProfileAdmin(admin.ModelAdmin):
+class DriverProfileAdmin(ModelAdmin):
     list_display = ("user", "vehicle_type", "status", "is_verified", "total_deliveries", "rating")
     list_filter = ("vehicle_type", "status", "is_verified")
 
 
 @admin.register(CustomerProfile)
-class CustomerProfileAdmin(admin.ModelAdmin):
+class CustomerProfileAdmin(ModelAdmin):
     list_display = ("user", "default_pickup_address", "default_dropoff_address", "preferred_payment_method")
 
 
 @admin.register(AdminProfile)
-class AdminProfileAdmin(admin.ModelAdmin):
+class AdminProfileAdmin(ModelAdmin):
     list_display = ("user", "department", "access_level")
 
 
 @admin.register(DriverDocument)
-class DriverDocumentAdmin(admin.ModelAdmin):
+class DriverDocumentAdmin(ModelAdmin):
     list_display = ("driver", "doc_type", "verified", "uploaded_at")
     list_filter = ("doc_type", "verified")
 
 
 @admin.register(DriverInvitation)
-class DriverInvitationAdmin(admin.ModelAdmin):
+class DriverInvitationAdmin(ModelAdmin):
     list_display = ("email", "full_name", "token", "expires_at", "accepted_at")
     readonly_fields = ("token",)
