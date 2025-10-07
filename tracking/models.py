854edc7e6b49497695e074b60892e198e61bf118
Mesh-254 <meshack1995@gmail.com>
2025-10-07 13:29:42 +0300

    updated str selfid

--- tracking/models.py
+++ tracking/models.py
@@ -92,7 +92,7 @@ class ProofOfDelivery(models.Model):
     created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return f"Proof for Booking {self.bookings.id}"
+        return f"Proof for Booking {self.booking.id}"
 
 
 class WebhookSubscription(models.Model):
