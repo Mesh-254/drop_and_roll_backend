18ef401118ec6d3d2c5abc447dee085febe99307
Mesh-254 <meshack1995@gmail.com>
2025-11-13 10:42:03 +0300

    Add periodic Celery Beat task to run optimize_bookings every minute

--- Drop_N_Roll/celery.py
+++ Drop_N_Roll/celery.py
@@ -1,7 +1,16 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Drop_N_Roll.settings")
 app = Celery("Drop_N_Roll")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
+
+
+app.conf.beat_schedule = {
+    'optimize-bookings-every-15-min': {
+        'task': 'bookings.tasks.optimize_bookings',
+        'schedule': crontab(minute='*/1'),  # Every 15 minutes
+    },
+}
