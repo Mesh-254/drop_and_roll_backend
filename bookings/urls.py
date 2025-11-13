4dfe31093d59f35c6ea3721f85d57208b0362523
Mesh-254 <meshack1995@gmail.com>
2025-11-13 10:32:23 +0300

    **Register  in API router with  endpoint**

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from bookings.api_views import QuoteViewSet, BookingViewSet, ShippingTypeViewSet, ServiceTypeViewSet, BookingStatusView, track_parcel
+from bookings.api_views import QuoteViewSet, BookingViewSet, RouteViewSet, ShippingTypeViewSet, ServiceTypeViewSet, BookingStatusView, track_parcel
 
 router = DefaultRouter()
 router.register(r"quotes", QuoteViewSet, basename="quotes")
 router.register(r"bookings", BookingViewSet, basename="bookings")
 router.register(r"shipping-types", ShippingTypeViewSet, basename="shipping-types")
 router.register(r"service-types", ServiceTypeViewSet, basename="service-type")
+router.register(r'routes', RouteViewSet, basename='routes')
 urlpatterns = [
     path("", include(router.urls)),
     path("booking-statuses/", BookingStatusView.as_view(), name="booking-statuses"),
